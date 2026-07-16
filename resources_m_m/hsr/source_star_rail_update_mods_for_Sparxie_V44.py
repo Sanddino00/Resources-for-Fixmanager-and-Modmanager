279c65817498bc2f023433578120ef70a9a93c0e
Sanddino00 <sanddino88@gmail.com>
2026-07-17 00:26:58 +0200

    .

---
+++ resources_m_m/hsr/source_star_rail_update_mods_for_Sparxie_V44.py
@@ -0,0 +1,150 @@
+# Made by Sora_
+# Thanks to the Genshin Impact Modding Community for their help and support
+
+# Fixes for Sparxie
+
+# ##########################################################################################################################
+#     ................                .... ..   ..               ............. ............                              
+#     ................                ... ...                    .........................                               
+#   ..............            ....                 ..            ........,]`.]............                               
+# ................            ......                             ...,/\]/O],*,[\[]........                               
+# ............                                           ........,/\@O@/,/\]]],`=`*,=\`...........                       
+# .......... .        ..                                 ....../@\@@/`/*`,/\/\[,*,,**=OO]]]........                      
+# ............                                        .. ..../O\O@[,,,///`********,*`,]\]*/\`.....                       
+# ...........  ...                                    .. .../O@@^*,/\/`*/\***********,**/*/\/.....                       
+# ............. .... .                     .  ............][\ooo\O\@^]/ooo`****,*****`*`^`/O@O..... ..                .. 
+# ............. .... .                   ............,O\OOO\oo]OOo@`@OO//`***,\\,,``/^,\^,***[\....                      
+# ............                            .............,//*,*]/oO^\@ooooo/^*]\]/`\*//**=**`[^*/^...                      
+# ............                            ...=]]]]]/[**`*,,*/\o@`O@O/OO\=O/[O\``[/^/``*O]`**,^\\....                     
+# ............ ...                       ......,OooooO/@@/o\[//=@o@o//,OOO/\//*/\OO\]`=\^*O]/@@\^..       .. .           
+# ............    ..                      ........[@O\oooo\O@/@OOO\///O/\\@^]@\OOO\\\oOO^\@o]OO\^..       .. .           
+# .......... .....    ..      .  .       ............/oo/\/,\@O@O@\OO\^]]@@/\,\//`=OO\O@^@O/=\\OO`....        ..         
+# ..... ....... ..... .       ....        .........,//O[[]OOOOOO/O/@@@@@@\/]o\OO`=O@\=OO/O/,=/^@@@....        ..         
+# ................            .....  .............=\`*`/\OOooO/@/@,O@@@@O^,@\OO*=O@\*oOOOO^=^/@/@,\... ..         .......
+# ................            ....        ......,//*,/OOOoO\=/OOo^\\.\O/.../O//\/@\O\@@\OO*@\O=\@..=.....         ....   
+# ................                    ........,/,/,//OO\/*,*@oOO\^O@......*O/`.**\@@@^@@@^/\O^**=^........               
+# ...,]/\]].....                      .....]\\*/O\@oo\/,`*/@Oo/@o^=^.....,.......@O@`/@@^@/oO`\OO^... .                  
+# .,/`@O[*............................,]/^**/@O/OOOO``*,\o@O/\OO/^=\..........[...,.*@O@@OO@,*/=/..                      
+# O,[,`\]/[\/OOO\/^[*,\/**,]]]]]]O\Oo\,*,/OO@@oOO`**,*,oO\/*@@@@/o,@\`..............@O@Oo@@`*/./...                      
+# \,/[,*`,\//@//oo\O]*,\`..\/**`,Oo/[OOo@O@\O/\//o\,`\//[*//@@@O@o`@^@`.[,......../@\@OO@@*=`......                      
+# O/@//\`*/**[\/\OO\@\\`\[\..\][\/@OO\OO^OO/]o/o/o\[/\*,/O/\@@@\O@O@,/@\.`..,]/@@OOO@@o@@/`.......                       
+# \/...,,[[\`*,*\\\/O[@.,//*./OOOO@@@OO@Ooo\o\@/\[,*\,OO@oo@@@@@O@@@=@@@@@@@@OO@=@@@OOO\O^.. ... .                       
+# .......*``],,``/\O]*/..\\^=\/oOo///\oOOOOO\]`*/`/]OO/oOO\O@@@@@@@@@@@@@@@@@O@@@@O\O@OO=^...     .. .                   
+# .../\\\\\=\/O\/[^\`=/...`.@\],/O`**\]`oooooo^O@\oo\@O@OOOOOOOO@@@@/@@@@@@@@@@\OO@@@/\/=.......  ..                     
+# ./OooooOO/^/^***/\,/@`....@/`,,*`*`*/]]O/@//ooOOO/@@@@O\OOOOOOOOOOOO@O]\@@@@O@@@@@@OO@OO^......         ....           
+# OooO/@\o\/*`******/@O^....@.*`*`***]/@@OOOOOOO/O@@OO\@@@@OOOOOO@@OO@OOOO@@@@@OOO@@@@@O@@O^.......               ....   
+# OOO\Oo\/**`**,**\]`@,=^**//\\/\\\`O\oO@/`/@O\@@OOOOOOOO@@@@OOO/@@@@@@@OOO@@@@OOOOO@@@@@@@O\..  .                . ..   
+# OOOOO@[,,*`]/Oooooo\Ooo\oo/O`/\@*[,[O@/O\OOO@@OOOOOO@OOOO@@@@OOOOO@@@@@O@@@/@\@@[OOOOO@@@@OO`...         .......       
+# OOOo/*`,,@oooo\oooo\//@\oo/O\//,\@OooOOOoo@O@OOOOO@@O@@@@@@@O@OOOOOO@@\\@@OO\OO@@`\/OOO\@@OO@...          . ....       
+# OOo@*``/\/[.......,.==@@OO/OO\/\^OO/@\OoO\@@@@@@@@@@@@@@@@@\@@@@@OOO@@@/@@OOOOO@@@@@OOOOOO@OO^...       ....           
+# OOOO,,/............^,@@O\@^@O/\//OO@@@@@@@@@@@@@@@@OOO/OOOOOO@O\@OOO@@@@@O]]\]]/O@\OOOOOOOOO@O...         ..           
+# =OO/.................@@OO@@@=]oo^OOO@@@@@OOOOOO/OO@O@@@@\OOOOOO@@@@/OOOOOO@@@@@@@@@@@OO@@@@@@@^.                       
+# ............,\O`.....,@O@@@OO@/\/OOOOO@@@OOOOOOOOOOOO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@^.                       
+# ............/@^........\/OOOOOO@@O/OOO@@OOOOOOOOOOOOOOOOOO\@O@@@@@@@@@@@@@@@@@@@@@@@@OOOOOO@@@^.                       
+# .. ... .....^*,].........\@O/@@@@@@@@@@@@@@@@@@OOOo/O@@@@@@O/o@@@@@@@@@@@@@@@OO@@@@@@@OOOOOOO@^.              ..       
+# ............\,***[].......=@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OOOOOoO\@@@@@@@@OO@O@OOOOOOO@@OOOOO@@@^.  ..        ......     
+# ... .........\,***,*[`......@oO@@@@@@@@@@@@@@@@@@@@@@\/OOoO/O/`*\@@@@@@@OOOOO@OOOOOOO\\OOOOOO@^..   ...       ..   .   
+# ....... .......\\,*``*,[]...,@OO@@@@@@@@@@@@@@@@@@[ooooo@/^*]/O\/\@@@@@OOOO@@@OOOOOOOOOO@OOOOO\..               ....   
+# .................,@@]*^,,*``**,\\@@@@@@@@@@@@@/@\oOO[]]/@\oooooooo\@@@@O@OOO@@O@@@OO\OOO@@OOOO@..               ...... 
+# ....    ............[@//]]`****]\]/@@@@@@@@@O@OOOooooooooooooOooO@\\/@@@@@@@OO/@@@@@@@@@OOOOOOO\....                   
+# ....  .... .... .......,\Oo//[]*^ooooooo\/@@ooOoooooOoooooOoO/`.//ooO\@@@@@@@/O@OO/@@@@@@@/OOOOO^...                   
+# .. ...  ....    ..   ......[\[`/=/oooooOooooooooOOOoo/OO[`...,/Ooo\\/./@@@@@@@@@OOOO\\@@@@@@@O@@@.....                  
+# ......  ....           ........[\O/[[[[[OO[[[[[\O/[`.......,O/\ooO`..=OO@@@@@@@@@OOOOOOOO@\@OO@@@@.... .               
+# ....    ....           .........,/\\......................///O/`......=@@@@@@@@@@OOOOOOOOOO@OO\@@@^.....               
+# ....       .            .........*=....................,[///`.........=@@@@@@@@@@OOOOOOOOOOO@O@@@@@.....               
+# ...... .                  ..   ...\\................,/][`.......... ..@@@@@@@@@@@@OOOOO@@@O\@@@@@@@@....               
+# ........                        ....\`.........]/OO[`.......  .......=@@@@@@@@@@@@@@@O@@@@@@@@@@@@@@^...               
+# . .... .... ....              ...........[`.............      .......@@@@@@@/OOO@@@@@@@@@@@@@@/@@@O@@^..               
+#   ......... ...             .   ..... ..................        ...,/@@@@@@@OOOOO/@@@@@@@OOO@@@@@OOOO@`.               
+#   ........  ....                    .    ......   ................/@@@@@@@@@@OOO@OOO@@@@@OOOOO@@@OOOOO@.........       
+# .   .....   ....                        .. ..  .         .. ..../@@@@@@@@@@@@@@O@@@@@@@@@@@@/O/@@OOOOOOO/`.... ..      
+#    ...    ..           .                                ....../OOO@@@@@@@@@@@@@O@@@@@@@@@@@@@@@=@@OOOO\/O/\.....       
+#    .... .           ....                               ...../@OOOOOO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OOOO\//O\\.....      
+# ........        ...... .                .           ....../@OOOOOOOO@@@@@@@@@@@@@@@@@@OoO@@@@@@@OOOOOOO/o`@]O....      
+# ........        .....                   ...         .....,@OOOOOO@@@@@O@=]/[[[@@@@@@@@@OOOOOOO@@OOOOOOOOo/o///`...     
+# ..  ...   ..                                    .....,/@@@@@OOO@@@@@@@@\oooooooooooo/o]\\\]]/@@@@OOOOOOO/]@[O`......   
+# ##########################################################################################################################
+
+import os
+import struct
+import argparse
+import shutil
+import re
+
+old_vs_new = {
+    # Sparxie
+    "337f7de8":"51288e6a",
+    "547d13f7":"b9250480",
+    "df489315":"12f315d4",
+    "a0dfb1ac":"54ee8b35",
+    "4dcbf0c8":"5c2a2064",
+    # Sparxie hair
+    "b7124f06":"d507f62a",
+    "979806ea":"3ef2cc18",
+    "f241b31a":"a4f51f30",
+    "67ec715e":"0e39f0ac",
+    "fa679826":"d734b734",
+}
+
+replacement_mapping = {
+    # 
+    "sora_33550336": {
+        "match_first_index = 33550336":"match_first_index = 33550337",
+    },
+}
+
+def process_folder(folder_path):
+    '''Process all the files in the folder and subfolders,
+    replacing the old values with the new ones.'''
+    for root, dirs, files in os.walk(folder_path):
+        for file in [x for x in files if x.lower().endswith('.ini') and x.lower() != 'desktop.ini']:
+            file_path = os.path.join(root, file)
+            print(f"--> Get in file '{file}' at '{file_path}'")
+            try:
+                with open(file_path, 'r', encoding="utf-8") as f:
+                    s = f.read()
+                    # old_stream = s
+                if '[disabled1234567890]' in s:
+                    print(f"Skipping file '{file}' as it already contains '[disabled1234567890]'")
+                    continue
+                modified = False
+                for old, new in old_vs_new.items():
+                    if old in s:
+                        s = s.replace(old, new)
+                        print(f" -  Hash '{old}' has been replase with '{new}'")
+                        modified = True
+                
+                for key, values in replacement_mapping.items():
+                    if key in s:
+                        for search_string, replace_string in values.items():
+                            if search_string not in s:
+                                print(f" -  Not contain the specified string '{search_string}'. Skipping current string")
+                                continue
+                            else:
+                                modified = True
+                                if callable(replace_string):
+                                    strr = replace_string(key)
+                                    s += f'\n\n{strr}'
+                                    print(f' -  {search_string[:4]}...{search_string[-4:]} --> {strr[:4]}...{strr[-4:]}')
+                                else:
+                                    s = s.replace(search_string, replace_string)
+                                    print(f' -  {search_string[:4]}...{search_string[-4:]} --> {replace_string[:4]}...{replace_string[-4:]}')
+                    else:
+                        print(f" -  key '{key}' not in")
+                if modified:
+                    backup_file_path = os.path.join(root, f"backup_{os.path.splitext(file)[0]}.txt")
+                    shutil.copy2(file_path, backup_file_path)
+                    print(f" -  Backup created: '{backup_file_path}'")
+                    with open(file_path, 'w', encoding="utf-8") as f:
+                        f.write(s)
+                        print(f'<-- File has been modified!')
+                else:
+                    print(f'<-- File had no matches. Skipping')
+            except Exception as e:
+                print(f'Error processing file: {file_path}')
+                print(e)
+                continue
+
+if __name__ == '__main__':
+    process_folder(os.getcwd())
+    input('Done!')
